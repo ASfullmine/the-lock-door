840311b5101bd1bc97447f30642bba71b9de374f
yu Hyth Soh <thetrueisouthere@outlook.com>
2025-07-29 01:57:51 +0200

    The Monolith ¿*?
    https://www.getmonero.org
    
    https://www.autoreseditores.com/alejandro.veintimilla?xcountry=es
    
    Alejandro Veintimilla
    Alejandro Veintimilla es un escritor apasionado por la ciencia ficción y el futuro. Tiene un trasfondo filosófico y poético que le da un valor único. Ecos y Cavernas es el primer libro que publica. Ha mantenido varios blogs desde el 2009, el último es Criptolibertad, un sitio web de tinte ciberpunk. También, es fundador de proyecto Academia Blockchain que cuenta con un canal de Youtube. Aunque estudió economía y filosofía, su tiempo es dedicado a la programación, a la escritura y a la música.
    
    Ecos y Cavernas
    
    Ecos y Cavernas es un viaje por una ciencia ficción diferente, llena de interrogantes que trascienden el mundo que conocemos hoy. Los varios cuentos se entrelazan en esquinas narrativas que remueven el piso de lo que podría ser el futuro y de remanentes de una literatura antigua que acompaña las historias. Los personajes envuelven al lector en una conversación con el tiempo. Los cuentos, entrelazados por una historiad e ideas y pasiones humanas, abren al lector a los dilemas filosóficos y teológicos que nos plantea la tecnología del mundo contemporáneo.
    
    "Hace mil años, había que viajar al Polo Norte para mirarlo; luego, cualquier persona pudo verlo en video en el antiguo Internet y conocer su brillo y su majestuosidad; después, solo hacía falta que un usuario se conectara a un programa para que viviera una simulación exacta del fenómeno. Ahora yo lo veo desde mi casa y usted lo ha vuelto a programar para Segunda Tierra. Los usuarios de su programa tendrán que volver a viajar al norte para mirar las luces verdes de la Aurora Boreal, y las admirarán como antes... Pero usted y yo, y todos los usuarios de La Red sabemos que esas luces son en verdad un conjunto de líneas de código, una imitación. ¿Vale la pena olvidarlo?"
    
    https://youtu.be/rlyA3bz97-0

---
+++ main/TheExam/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+//._∆~ 
+
+[assembly: AssemblyTitle(".-∆~ The Exam")]
+[assembly: AssemblyDescription(".-∆~ The Exam")]
+
+[assembly: AssemblyCompany(".-∆~")]
+[assembly: AssemblyProduct(".-∆~ The Exam")]
+[assembly: AssemblyCopyright(".-∆~ ")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
